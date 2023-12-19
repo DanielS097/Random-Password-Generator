0c88c1fb1c9335022350f1026691fb774cdf6471
Daniel <danielstoica078@gmail.com>
2023-12-19 16:49:13 +0000

    added the function to prompt users

--- script.js
+++ script.js
@@ -94,82 +94,17 @@ let numbers;
 let specialChars;
 let tooLong;
 let tooShort;
-let lenght;
+let length;
+let password;
+let i;
+let randomIndex;
+let randomCharacter;
 // Function to prompt user for password options
 function getPasswordOptions() {
-    lenght = prompt("Please select the lenght of your password, between 8 and 128 characters.");
-    //! Prompt for password length
-    //! At least 8 characters, no more than 128 characters
-    //! Conditional to check that the number that was entered is in range
-    // Prompts store data as strings, so need to parse into a number
-    // If the user's input is out of range, either return out of the function or call the function again
+     length = prompt("Please select the lenght of your password, between 8 and 128 characters.");
+    
+};
 
-    // Confirm which character sets to use
-    //   If the user answers false for all, either return out of the function or call the function again
-
-    //   Once they select a character set:
-    //   Generate a random character for each selected character set
-    //   Either push selected character sets to a mega-array of all selected characters
-    //   OR you can keep the arrays separate and generate a random number to select the array and another to select the index
-
-    //   Once character sets are selected, move on to generating random characters
-}
-
-
-
-// Function for getting a random element from an array
-function getRandom(arr) {
-
-    // Need a variable to hold the password as it's being generated
-    // Need a variable to hold the index that's being generated
-
-    // For loop that loops the number of times that matches the length the user chose
-    // Generate a random number
-    // That number is the index for a character in the mega-array
-    // So then, mega-array[generated-index] is the actual character
-    // Add that character to the password
-
-    // Once we finish the for loop, return the generated password
-}
-
-// Function to generate password with user input
-function generatePassword() {
-
-
-}
-// Get references to the #generate element
-const generateBtn = document.querySelector('#generate');
-
-// Write password to the #password input
-function writePassword() {
-    let lenght = prompt("Please select the lenght of your password, between 8 and 128 characters.");
-    if (lenght !== null) {
-        if (lenght > 128) {
-            tooLong = alert("Character choice is too long, please choose between 8 and 128");
-            writePassword();
-        };
-        if (lenght < 8) {
-            tooShort = alert("Character choice is too short, please choose between 8 and 128");
-            writePassword();
-        }
-        upperCase = confirm("Would you like uppercase characters in your password?");
-        lowerCase = confirm("Would you like lowercase characters in your password?");
-        numbers = confirm("Would you like numbers characters in your password?");
-        specialChars = confirm("Would you like special characters in your password?");
-
-    };
-    const password = generatePassword();
-    const passwordText = document.querySelector('#password');
-    console.log(lenght);
-    console.log(upperCase);
-    console.log(lowerCase);
-    console.log(numbers);
-    console.log(specialChars);
-    passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
 
 
 
